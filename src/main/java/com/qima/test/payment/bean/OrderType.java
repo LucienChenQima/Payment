f6d27dd7637f9f802369c54af46c3cf9a0357139
Bozhou Yu <bozhou.yu@qima.com>
2021-08-18 10:25:44 +0800

    fix data field typo

--- src/main/java/com/qima/test/payment/bean/OrderType.java
+++ src/main/java/com/qima/test/payment/bean/OrderType.java
@@ -1,10 +1,13 @@
 package com.qima.test.payment.bean;
 
+import lombok.Getter;
+
 public enum OrderType {
     ON_LINE("online"),
     OFF_LINE("offline"),
     INTERNAL("internal");
 
+    @Getter
     private String name;
     private OrderType(String name) {
         this.name = name;
